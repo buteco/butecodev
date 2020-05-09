b1a9a2b5f058293320546c97bfef6ba2c71f8a4e
George Y. Kussumoto <georgeyk.dev@gmail.com>
2020-05-09 17:44:59 -0300

    Home page fixes

--- butecodev/assets/static/js/main.js
+++ butecodev/assets/static/js/main.js
@@ -35,32 +35,32 @@
 	// Nav.
 
 		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo h1').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
+			//$(
+				//'<div id="titleBar">' +
+					//'<a href="#navPanel" class="toggle"></a>' +
+					//'<span class="title">' + $('#logo h1').html() + '</span>' +
+				//'</div>'
+			//)
+				//.appendTo($body);
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
+		//// Panel.
+			//$(
+				//'<div id="navPanel">' +
+					//'<nav>' +
+						//$('#nav').navList() +
+					//'</nav>' +
+				//'</div>'
+			//)
+				//.appendTo($body)
+				//.panel({
+					//delay: 500,
+					//hideOnClick: true,
+					//hideOnSwipe: true,
+					//resetScroll: true,
+					//resetForms: true,
+					//side: 'left',
+					//target: $body,
+					//visibleClass: 'navPanel-visible'
+				//});
 
 })(jQuery);
